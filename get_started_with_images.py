cdaaf0e0e1808e379d6981049aa4501391c99f1d
Harsh <harshb15003@gmail.com>
2024-05-30 13:18:06 +0530

    motion detection

---
+++ get_started_with_images.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("lena.jpg",0) 
+print(img)
+
+cv2.imshow("Lena", img)
+k = cv2.waitKey(0) & 0xFF
+if k == 27:
+  cv2.destroyAllWindows()
+elif k==ord('s'):
+  cv2.imwrite("lena_copy.png",img)
+  cv2.destroyAllWindows()
